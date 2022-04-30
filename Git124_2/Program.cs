41c6e3eac6bfdb4e19d2a15820c47ff761eab2b2
IsmayilovaSara <ismayilovassara@gmail.com>
2022-04-30 16:54:21 +0400

    TEST

--- Git124_2/Program.cs
+++ Git124_2/Program.cs
@@ -6,7 +6,7 @@ namespace Git124_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello `1235523432eSWorld!");
         }
     }
 }
