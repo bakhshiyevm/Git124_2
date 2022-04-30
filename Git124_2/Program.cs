c9801f767be14af669032fcc80b4537c492dba85
BakhshiyevM <mbakhshiyev@pasha-insurance.az>
2022-04-30 16:36:29 +0400

    changed

--- Git124_2/Program.cs
+++ Git124_2/Program.cs
@@ -6,7 +6,7 @@ namespace Git124_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello 123");
         }
     }
 }
