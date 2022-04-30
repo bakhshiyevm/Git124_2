a61464a34a8c4e832a184f8bc341617c438d29fb
BakhshiyevM <mbakhshiyev@pasha-insurance.az>
2022-04-30 17:26:16 +0400

    commit1

--- Git124_2/Program.cs
+++ Git124_2/Program.cs
@@ -4,10 +4,11 @@ namespace Git124_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] a123123)
         {
-            Console.WriteLine("hello m124");
-            Console.WriteLine("aliiiiii");
+            Console.WriteLine("123123123");
+            Console.WriteLine("12312123");
         }
+        //testtest
     }
 }
