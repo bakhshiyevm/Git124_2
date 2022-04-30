25bbc4aeae98ce1fa21f69f1426b5ac0195d8956
SananSadiyev <sadiyev42@mail.ru>
2022-04-30 17:26:23 +0400

    commit2

--- Git124_2/Program.cs
+++ Git124_2/Program.cs
@@ -4,10 +4,12 @@ namespace Git124_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] a223344)
         {
             Console.WriteLine("hello m124");
-            Console.WriteLine("aliiiiii");
+            Console.WriteLine("Salammmm");
         }
+        //test
     }
+
 }
